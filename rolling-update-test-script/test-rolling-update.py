b032504cd3623bfe5f3c33d6354c7353f43aed2c
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2020-06-22 17:58:05 +0530

    Update test-rolling-update.py

--- rolling-update-test-script/test-rolling-update.py
+++ rolling-update-test-script/test-rolling-update.py
@@ -2,8 +2,8 @@ import requests
 
 # make 200 requests to the server
 output = open("output_response_after_rolling_update.txt", "a+")
-old_code_response = "Hello World. <br/>This is Gaurav Sharma Udacity Devops-nanodegree capstone project"
-new_code_response = "This is Gaurav Sharma Udacity Devops-nanodegree capstone project"
+old_code_response = "Hello World. <br/>This is Gopal Khatri Devops-nanodegree capstone project"
+new_code_response = "Welcome to my Zone"
 response_texts = []
 for i in range(600):
     response = requests.get("http://Devops-173062868.us-east-1.elb.amazonaws.com")
